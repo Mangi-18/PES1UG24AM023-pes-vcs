0fb17f44a3a0324df8c76abe6d9a767571f21071
Akhil <akhil@example.com>
2026-04-21 07:23:13 +0000

    phase 3 building tree from index

--- commit.c
+++ commit.c
@@ -193,9 +193,17 @@ int head_update(const ObjectID *new_commit) {
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
+
 int commit_create(const char *message, ObjectID *commit_id_out) {
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0) {
+        fprintf(stderr, "error: nothing staged to commit\n");
+        return -1;
+    }
+
     (void)message;
     (void)commit_id_out;
     return -1;
 }
+
 
