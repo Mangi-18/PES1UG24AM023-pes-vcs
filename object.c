b58da14fb55e3eae4d6a7d03f62499e1793b987b
Akhil <akhil@example.com>
2026-04-21 06:11:23 +0000

    Phase 1: start object_write -added header creation logic

--- object.c
+++ object.c
@@ -95,8 +95,13 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
-    return -1;
+    const char *type_str = (type == OBJ_BLOB) ? "blob" :
+                       (type == OBJ_TREE) ? "tree" : "commit";
+
+char header[64];
+int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
+
+
 }
 
 // Read an object from the store.
